f72ef81a33dd4b0509fd5da29cfd83577efcef71
kittybellth <kittybell.th@gmail.com>
2018-03-12 16:07:21 -0500

    deleted some text

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ import '../styles/welcome.css';
 export default props => (
     <Segment className="jumbotron" basic loading={props.loading}>
         <h1 className="welcome-text">
-        Welcome InRhythm Team!
+            Welcome !
         </h1>
     </Segment>
 );
