1310fb5f991649e8c6e3c8a1473127b94ad43380
kittybellth <kittybell.th@gmail.com>
2018-03-11 21:19:28 -0500

    fixed defaultURL for github page

--- src/router.js
+++ src/router.js
@@ -6,11 +6,13 @@ import App from './containers/App';
 import AlbumContainer from './containers/AlbumContainer';
 import Nomatch from './components/Nomatch';
 
+const defaultURL = process.env.PUBLIC_URL;
+
 export default ()=> (
     <Router history={history} >
         <Switch >
-            <Route exact path="/" component={App} />
-            <Route exact path="/:artist" component={AlbumContainer} />
+            <Route exact path={`${defaultURL}/`} component={App} />
+            <Route exact path={`${defaultURL}/:artist`} component={AlbumContainer} />
             <Route path="*" component={Nomatch} />
         </Switch>
     </Router>
